c30dc38a0cef10b029ac82e3bd9eeaa1ad067fbf
jimtom2713 <jwilliamson528@gmail.com>
2015-07-10 20:55:37 -0700

    updated collision

--- client/watchout.js
+++ client/watchout.js
@@ -3,23 +3,32 @@
 
 //build the game area
 d3.select('body').append('svg').attr('class', 'game-area');
+var playerXY = {x:400,y:300};
 
 // console.log(player);
 
-var checkCollision = function (enemyCoordinate) {
-  var userX = d3.select('.player')[0][0].cx.baseVal.value;
-  var userY = d3.select('.player')[0][0].cy.baseVal.value;
+var checkCollision = function () {
+  // var userX = d3.select('.player')[0][0]['cx']['baseVal']['value'];
+  // var userY = d3.select('.player')[0][0]['cy']['baseVal']['value'];
+  var userX = playerXY.x;
+  var userY = playerXY.y;
+  // console.log('running');
   // player[0][0].cx.baseVal.value
+  // player[0][0]['cx']['baseVal']['value']
   var enemyCoordinate = d3.select('svg').selectAll('.enemy');
+  // console.log('in for loop');
+  // console.log(enemyCoordinate);
   for(var i = 0; i < enemyCoordinate[0].length; i++){
+    // console.log('checking');
     var tempX = Math.pow((userX - enemyCoordinate[0][i].__data__.cx) , 2);
     var tempY = Math.pow((userY - enemyCoordinate[0][i].__data__.cy) , 2);
-    if(Math.sqrt(tempX + tempY) < 10 ){
+    var temp = tempX + tempY;
+    // console.log(tempX - tempY);
+    if(Math.sqrt(temp) < 20 ){
       console.log('Collission');
     }
   }
 }
-
 var drag = d3.behavior.drag()  
              .on('dragstart', function() { player.style('fill', 'blue');
                 // checkCollision(d3.select('svg').selectAll('.enemy'));
@@ -27,7 +36,8 @@ var drag = d3.behavior.drag()
              .on('drag', function() { player.attr('cx', d3.event.x)
                                             .attr('cy', d3.event.y); 
                                             // checkCollision(d3.select('svg').selectAll('.enemy'));
-                                            
+                                      playerXY.x = d3.event.x;
+                                      playerXY.y = d3.event.y;
                                             // console.log(enemyCoordinate[0][1].__data__.cx);
                                      })
              .on('dragend', function() { player.style('fill', 'red'); 
@@ -40,15 +50,11 @@ var player = d3.select('svg').append('svg').attr('class', 'player').append('circ
                   .attr("r", 20)
                   .attr("fill", 'red' )
                   .call(drag);    
-// var player = d3.select('.player').call(drag);
-// debugger;
-// console.log(player);
-// var drag = d3.behavior.drag();
-// player.call(drag);
+
 
 function update(){
   var storageArray = [];
-  for (var i = 0; i < 25; i++){
+  for (var i = 0; i < 1; i++){
     var x = Math.random()*800;
     var y = Math.random()*600;
     var enemyCoordinate = {
