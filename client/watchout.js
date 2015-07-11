d74aebadb2ac26bc70875518cc661015ad16c7f2
jimtom2713 <jwilliamson528@gmail.com>
2015-07-10 19:12:38 -0700

    finished dragable user

--- client/watchout.js
+++ client/watchout.js
@@ -4,55 +4,52 @@
 //build the game area
 d3.select('body').append('svg').attr('class', 'game-area');
 
-// var generateCoordinates = function () {
-//   // console.log('made');
-//   var storageArray = [];
-//   for (var i = 0; i < 10; i++){
-//     var x = Math.random()*800;
-//     var y = Math.random()*600;
-//     var enemyCoordinate = {
-//       cx : x,
-//       cy : y,
-//       // r : 10
-//     };
-//     storageArray.push(enemyCoordinate);
-//   }
-//   // console.log(storageArray)
-//   return storageArray;
-// };
+// console.log(player);
+
+var drag = d3.behavior.drag()  
+             .on('dragstart', function() { player.style('fill', 'blue'); })
+             .on('drag', function() { player.attr('cx', d3.event.x)
+                                            .attr('cy', d3.event.y); })
+             .on('dragend', function() { player.style('fill', 'red'); });
+
+var player = d3.select('svg').append('svg').attr('class', 'player').append('circle')
+                  .attr("cx", 400)
+                  .attr("cy", 300)
+                  .attr("r", 20)
+                  .attr("fill", 'red' )
+                  .call(drag);    
+// var player = d3.select('.player').call(drag);
+
+
+// var drag = d3.behavior.drag();
+// player.call(drag);
 
 function update(){
-  // console.log('updating');
   var storageArray = [];
-  for (var i = 0; i < 10; i++){
+  for (var i = 0; i < 25; i++){
     var x = Math.random()*800;
     var y = Math.random()*600;
     var enemyCoordinate = {
       cx : x,
       cy : y,
-      // r : 10
     };
     storageArray.push(enemyCoordinate);
   }
-  // var existing = gameBoard.selectAll('.enemy')
-  //       .data(data);
-
-  d3.select('svg').selectAll('.enemy').data(storageArray).enter().append('circle')
-                  .attr('class', 'enemy')
+  var existing = d3.select('svg').selectAll('.enemy').data(storageArray);
+  existing.enter().append('circle')
+                  .attr('class', 'enemy');
+  existing.transition().duration(1000)
                   .attr("cx", function(d) { return d.cx; })
                   .attr("cy", function(d) { return d.cy; })
-                  .attr("r", 10);
-  // existing.exit().remove();
-  // storageArray = [];
+                  .attr("r", 10);             
 }
-// update();
-// update(generateCoordinates());
-
-// function keepRunning(){
-//   // console.log('here'); 
-//   var x = generateCoordinates();
-//   update(x); 
-// }
 
 
-setInterval(update, 100);
+
+setInterval(update, 1000);
+
+
+//Make a differently-colored dot to represent the player. Make it draggable.
+//Detect when a enemy touches you.
+//Keep track of the user's score, and display it.
+//Use css3 animations to make the enemies whirling shuriken.
